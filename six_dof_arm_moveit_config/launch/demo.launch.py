f3331768b6cedea0fca4f528e9e9dd0a82c20356
Ghada <ghadanoor.el@gmail.com>
2025-10-17 13:00:27 +0500

    Add files via upload

--- six_dof_arm_moveit_config/launch/demo.launch.py
+++ six_dof_arm_moveit_config/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("six_dof_arm", package_name="six_dof_arm_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("fanuc", package_name="six_dof_arm_moveit_config").to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- six_dof_arm_moveit_config/launch/spawn_controllers.launch.py
+++ six_dof_arm_moveit_config/launch/spawn_controllers.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_spawn_controllers_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("six_dof_arm", package_name="six_dof_arm_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("fanuc", package_name="six_dof_arm_moveit_config").to_moveit_configs()
     return generate_spawn_controllers_launch(moveit_config)
